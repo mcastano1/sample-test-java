7a92a41d513fc8b5b29050bc9bf15e2f1fc67bd5
MCastano4 <monicaj.castano@gmail.com>
2025-01-08 16:52:18 -0500

    test commit

--- src/main/java/com/hackerrank/sample/SampleApplication.java
+++ src/main/java/com/hackerrank/sample/SampleApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SampleApplication {
 
+
+    //comments test commit
     public static void main(String[] args) {
         SpringApplication.run(SampleApplication.class, args);
     }
